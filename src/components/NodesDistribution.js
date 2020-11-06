0e2c2cef85b73fc7808811d083b2d86e47c60fbe
Maxim Antonov <helloto@me.com>
2020-11-06 15:28:03 +0200

    Custom map + get it from lokinet

--- src/components/NodesDistribution.js
+++ src/components/NodesDistribution.js
@@ -4,19 +4,35 @@ import { TrapApiError, Widget, WidgetHeader, WidgetBody, WidgetLoader } from '@m
 import Map from 'pigeon-maps'
 import MapMarker from './MapMarker'
 import computeRequestId from '../lib/computeRequestId'
-import innerCss from './css/nodes-distribution';
+import innerCss from './css/nodes-distribution'
 import graphTheme from '../lib/graphTheme'
 
 const Marker = ({ left, top, style, children }) => (
-    <div style={{
-      position: 'absolute',
-      left: left - 15,
-      top: top - 30,
-      width: 30,
-      height: 30,
-      ...(style || {})
-    }}>{children}</div>
-  )
+    <div
+        style={{
+            position: 'absolute',
+            left: left - 15,
+            top: top - 30,
+            width: 30,
+            height: 30,
+            ...(style || {}),
+        }}
+    >
+        {children}
+    </div>
+)
+
+let tileHost =
+    window &&
+    window.location &&
+    window.location.hostname &&
+    window.location.hostname.includes('.loki')
+        ? 'http://o8fzpgsnhxfkpu6ktjtiaeg61obu158rt19z6jfhpycn6a5hg3by.loki:8080'
+        : 'http://lokidashboard.com:8080'
+
+function mapTilerProvider(x, y, z, dpr) {
+    return `${tileHost}/styles/klokantech-basic/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.png`
+}
 
 export default class NodesDistribution extends Component {
     static propTypes = {
@@ -29,34 +45,47 @@ export default class NodesDistribution extends Component {
 
     static getApiRequest({ url, headers = {} }) {
         return {
-            id: computeRequestId(url, headers), 
+            id: computeRequestId(url, headers),
             params: { url, headers },
         }
     }
 
     render() {
         const { title, url, apiData, apiError } = this.props
-                
-        const theme = graphTheme(this.props);
 
-        const body = (apiData && !apiError) ? (
-        <div style={{ position: 'relative', width: '100%', height: '100%'}}>
-            <Map defaultCenter={[20.674, 12.403]} defaultZoom={2.3}>
-                {
-                    apiData.map((node, index) => (
-                    <Marker key={index} anchor={[node.location.latitude, node.location.longitude]}>
-                        <MapMarker theme={theme} ip={node.ip} lastProofSecondsAgo={node.lastProofSecondsAgo}/>
-                    </Marker>
-                    ))
-                }
-            </Map>
-        </div>) : (<WidgetLoader />);
-        
+        const theme = graphTheme(this.props)
+
+        const body =
+            apiData && !apiError ? (
+                <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+                    <Map
+                        provider={mapTilerProvider}
+                        defaultCenter={[20.674, 12.403]}
+                        defaultZoom={2.3}
+                    >
+                        {apiData.map((node, index) => (
+                            <Marker
+                                key={index}
+                                anchor={[node.location.latitude, node.location.longitude]}
+                            >
+                                <MapMarker
+                                    theme={theme}
+                                    ip={node.ip}
+                                    lastProofSecondsAgo={node.lastProofSecondsAgo}
+                                />
+                            </Marker>
+                        ))}
+                    </Map>
+                </div>
+            ) : (
+                <WidgetLoader />
+            )
+
         return (
             <Widget>
                 <WidgetHeader title={title || url} />
                 <WidgetBody>
-                    <style dangerouslySetInnerHTML={{__html: innerCss }} />
+                    <style dangerouslySetInnerHTML={{ __html: innerCss }} />
                     <TrapApiError error={apiError}>{body}</TrapApiError>
                 </WidgetBody>
             </Widget>
