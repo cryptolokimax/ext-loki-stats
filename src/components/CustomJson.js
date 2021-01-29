3c008acd53c2049d61b2e5e531e4c154a202c6f8
Maxim Antonov <helloto@me.com>
2021-01-29 16:13:50 +0300

    Custom json to check apidata

--- src/components/CustomJson.js
+++ src/components/CustomJson.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import _ from 'lodash'
 import { TrapApiError, Widget, WidgetHeader, WidgetBody, WidgetLoader } from '@mozaik/ui'
 import computeRequestId from '../lib/computeRequestId'
-import innerCss from './css/inner';
+import innerCss from './css/inner'
 
 const createMarkup = (template, data) => ({
     __html: template(data),
@@ -30,16 +30,21 @@ export default class CustomJson extends Component {
         const { title, url, apiData, apiError, template } = this.props
 
         let body = <WidgetLoader />
-        if (apiData && !apiError) {
+        if (apiData && !_.isEmpty(apiData) && !apiError) {
             const compiled = _.template(template)
-            body = <div style={{ width: '100%', height: '100%' }} dangerouslySetInnerHTML={createMarkup(compiled, apiData)} />
+            body = (
+                <div
+                    style={{ width: '100%', height: '100%' }}
+                    dangerouslySetInnerHTML={createMarkup(compiled, apiData)}
+                />
+            )
         }
 
         return (
             <Widget>
                 <WidgetHeader title={title || url} />
                 <WidgetBody>
-                    <style dangerouslySetInnerHTML={{__html: innerCss }} />
+                    <style dangerouslySetInnerHTML={{ __html: innerCss }} />
                     <TrapApiError error={apiError}>{body}</TrapApiError>
                 </WidgetBody>
             </Widget>
